7f7c97d3bb42280aa72404bb9804e28efe3a44a9
Coderx8972 <s89722020@gmail.com>
2025-04-09 10:21:23 +0530

    solve these

--- Main.java
+++ Main.java
@@ -1,72 +1,21 @@
-package dsa;
-import java.util.*;
-class Main {
-    public static int minimumIndex(List<Integer> nums) {
-        int dominant = findDominantElement(nums);
-        if (dominant == -1) return -1;
 
-        int leftCount = 0, totalDominantCount = 0;
-        for (int num : nums) {
-            if (num == dominant) {
-                totalDominantCount++;
-            }
-        }
+import java.util.Scanner;
 
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums.get(i) == dominant) {
-                leftCount++;
-            }
 
-            int leftSubarrayCount = leftCount;
-            int rightSubarrayCount = totalDominantCount - leftCount;
-
-            if (leftSubarrayCount > (i + 1) / 2 &&
-                    rightSubarrayCount > (nums.size() - i - 1) / 2) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
-    static int findDominantElement(List<Integer> arr) {
-        int candidate = -1, count = 0;
-
-        // Boyer-Moore Majority Vote algorithm
-        for (int num : arr) {
-            if (count == 0) {
-                candidate = num;
-                count = 1;
-            } else if (num == candidate) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-
-        int totalCount = 0;
-        for (int num : arr) {
-            if (num == candidate) {
-                totalCount++;
-            }
-        }
-
-        return (totalCount > arr.size() / 2) ? candidate : -1;
-    }
-
+public class Main {
     public static void main(String[] args) {
-        List<Integer> l = new ArrayList<>();
-        l.add(2);
-        l.add(1);
-        l.add(3);
-        l.add(1);
-        l.add(1);
-        l.add(1);
-        l.add(7);
-        l.add(1);
-        l.add(2);
-        l.add(1);
-        int ans=minimumIndex(l);
-        System.out.println("answer :"+ans);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = scanner.next();
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) > s.charAt(i + 1)) {
+                System.out.println("YES");
+                System.out.println((i + 1) + " " + (i + 2));
+                return;
+            }
+        }
+
+        System.out.println("NO");
     }
 }
